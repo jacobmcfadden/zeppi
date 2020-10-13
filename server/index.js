6c7de34f78b7130d4d77ba47c987482f9056cdb1
jacobmcfadden <jacobmcfadden@protonmail.com>
2020-10-13 16:35:14 -0500

    finished adds

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ const friendCtrl= require('./controllers/friendController');
 const app = express();
 
 const {CONNECTION_STRING, SESSION_SECRET, SERVER_PORT}=process.env;
+
 app.use(express.json())
+app.use( express.static( `${__dirname}/../build` ) );
 app.use(session({
     resave: false,
     saveUninitialized: true,
